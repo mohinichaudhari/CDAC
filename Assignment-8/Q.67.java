53f8141151d7477e82ca41380563889e4fd683c7
mohinichaudhari <35225683+mohinichaudhari@users.noreply.github.com>
2020-10-07 00:03:31 +0530

    Update Q.67.java

--- Assignment-8/Q.67.java
+++ Assignment-8/Q.67.java
@@ -1,41 +1,33 @@
-/* 63. Write a program to count how many times character ‘t’ occurs in a file. 
- */
-
 package p61_67;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
-public class q63 {
+public class q67 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		// TODO Auto-generated method stub
-		File f = new File("D:\\Adinath\\ACDAC\\java\\prac\\file1.txt");
-		if(!f.exists()){
-			f.createNewFile();
-			
-		}
-		FileWriter f1 = new FileWriter("D:\\Adinath\\ACDAC\\java\\prac\\file1.txt");
-		String str = "how are you t and t are u there t ttt";
-		BufferedWriter b1 = new BufferedWriter(f1);
-		f1.write(str);
-		b1.flush();
-		b1.close();
-		FileReader r1 = new FileReader("D:\\Adinath\\ACDAC\\java\\prac\\file1.txt");
-		int ch ;
-		int count = 0;
-		while((ch = r1.read()) != -1) {
-			if((char)ch == 't') {
-				count++;
-			}
-		}
-		System.out.println("Count : "+ count);
-		f1.close();
-	
+		FileInputStream f = new FileInputStream("D:\\Adinath\\ACDAC\\java\\prac\\file1.txt");
+		ObjectInputStream o = new ObjectInputStream(f);
 		
-	}
+		ArrayList<Item> a = new ArrayList<>();
+		Object obj = null;
+	
+boolean flag = true;
+		while(flag) {
+			if(f.available() != 0) {
+				obj = (Item)o.readObject();
+				a.add((Item) obj);
+			}else {
+				flag = false;
+			}
+	    }
+		for(Item x : a) {
+			System.out.println(x);
+		}
 
+		o.close();
+}
 }
